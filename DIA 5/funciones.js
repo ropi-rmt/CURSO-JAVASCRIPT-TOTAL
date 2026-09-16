52174a5e6dee7b1cac88c7cfd1e919cf81138f98
ropi-rmt <ropi.rmt@gmail.com>
2026-09-16 14:58:03 -0300

    formato de funciones.js

--- DIA 5/funciones.js
+++ DIA 5/funciones.js
@@ -1,34 +1,34 @@
-function recomendarPelicula(genero){
+function recomendarPelicula(genero) {
     let mensaje = analizarPelicula(genero);
 
-    let elementoRecomendacion =document.getElementById("recomendacion");
+    let elementoRecomendacion = document.getElementById("recomendacion");
 
-    elementoRecomendacion.textContent= "Nuestra recomendacion es " + mensaje;
+    elementoRecomendacion.textContent = "Nuestra recomendacion es " + mensaje;
 }
 
-function analizarPelicula(genero){
+function analizarPelicula(genero) {
 
     let mensaje;
 
-    let edad =Number(document.getElementById("inputEdad").value);
+    let edad = Number(document.getElementById("inputEdad").value);
 
-    switch(genero){
+    switch (genero) {
 
         case "comedia":
-            if (edad<16){
-                mensaje="Todopoderoso";
+            if (edad < 16) {
+                mensaje = "Todopoderoso";
 
-            }else{
-                mensaje="Qué pasó anoche?";
+            } else {
+                mensaje = "Qué pasó anoche?";
             }
             break;
 
-            case "terror":
-            if (edad<16){
-                mensaje="No hay peliculas disponibles de este género para menores de 16";
+        case "terror":
+            if (edad < 16) {
+                mensaje = "No hay peliculas disponibles de este género para menores de 16";
 
-            }else{
-                mensaje="Hell House LLC";
+            } else {
+                mensaje = "Hell House LLC";
             }
             break;
 
